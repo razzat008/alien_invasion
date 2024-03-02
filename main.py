53af43c3c0f5dcaccf6a71939ac698b2d8959beb
Rajat Dahal <radiusrazzat111@gmail.com>
2024-03-02 10:30:48 +0545

    added: check up/down events

--- main.py
+++ main.py
@@ -8,18 +8,22 @@ from ship import Ship
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        x = self.settings = Settings()
-
+        self.settings = Settings()
+        """to make the game fullscreen"""
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         self.screen = pygame.display.set_mode(
-            (x.screen_width, x.screen_height))
+            (self.settings.screen_width, self.settings.screen_height))
 
-        self.bg_color = x.bg_color
+        self.bg_color = self.settings.bg_color
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
 
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _update_screen(self):
@@ -29,25 +33,36 @@ class AlienInvasion:
         pygame.display.flip()
 
     def _check_events(self):
-        """Respond to keypressess and mouse events"""
+        """Respond to keypress and mouse events"""
         for event in pygame.event.get():
-            if pygame.display.get_focused():
-                print("Window has focus")
-            else:
-                print("Window does not have focus")
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 self._check_down_events(event)
-
             elif event.type == pygame.KEYUP:
                 self._check_up_events(event)
 
     def _check_down_events(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.x += self.
+        if event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
 
     def _check_up_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
 
 if __name__ == "__main__":
