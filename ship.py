7d5db8bdd9fcaf9252f1c984963b2bb82ecd8fae
Razzat <radiusrazzat111@gmail.com>
2024-09-25 23:09:18 +0545

    center ship when die

--- ship.py
+++ ship.py
@@ -45,3 +45,8 @@ class Ship:
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
