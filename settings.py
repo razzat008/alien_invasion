8bdade87c07f58864ffb15772a0d53f48c9b86e3
Razzat <radiusrazzat111@gmail.com>
2024-09-25 23:09:55 +0545

    buttons and minor settings

--- settings.py
+++ settings.py
@@ -1,17 +1,18 @@
 class Settings:
     def __init__(self):
-        self.screen_width = 1920  # 1200
-        self.screen_height = 1080  # 800
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
         # self.bg_color = (0, 0, 0)
 
         # speed of the ship
         self.ship_speed = 15
+        self.ships_limit = 3
 
         # properties of bullet
-        self.bullet_speed = 1.5
+        self.bullet_speed = 5.5
         self.bullet_color = (230, 0, 0)
-        self.bullet_width = 800
+        self.bullet_width = 8
         self.bullet_height = 18
         self.bullets_allowed = 9
 
